de151cdde56ed3336fdb9a3d94ef808c9297c1bd
Elias Medawar <elias.medawr@liiva.ch>
2023-03-24 11:15:45 +0100

    Basics comments / implementing get_sitemaps scripts

--- scraping/helpers/get_sitemaps.py
+++ scraping/helpers/get_sitemaps.py
@@ -8,68 +8,37 @@ from os.path import isfile, join
 import csv
 import time
 
-def get_text_from_url(url):
+def get_links_from_url(url,base_url, links):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        return soup
+        extract_links(soup, base_url,links)
+
     else:
         print(f"Error: {response.status_code}")
         return None
 
+def extract_links(soup, base_url,links):
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href:
+            full_url = urljoin(base_url, href)
+            links.append([full_url])
+    return links              
 
-def get_question_answers_from_url(url,qa):
-    print(f"Visiting: {url}")
-    soup = get_text_from_url(url)
-    if soup:
-        parse_question_answer_from_html(soup,qa, url)
-    
-
-def parse_question_answer_from_html (web_response,qa,url):
-    domain_eak ="eak.admin.ch";
-    domain_ahv_iv =  "ahv-iv.ch";
-    add_your_logic ="my-domain.ch"
-    if domain_eak in url: 
-        soup =web_response.find("div", { "id" : "content" })
-        for article in soup.find_all('article'):
-            question =article.find("h2");
-            answer= article.find("p");
-            if( question is not None and answer is not None):
-                new_qa = {"frage": question.get_text(), "antwort": answer.get_text() , "url":url}
-                qa.append(new_qa)
-    elif domain_ahv_iv in url: 
-        soup = web_response.find("div", { "class" : "co-questionanswer" })
-        if(soup is not None):
-            for article in soup.find_all("div",{ "class" :'sc-element'}):
-                question =article.find("h3", {"class":"co-toggle-title"});
-                answer= article.find("div",{"class":"co-questionanswer-content"});
-                if( question is not None and answer is not None):
-                    new_qa = {"frage": question.get_text(), "antwort": answer.get_text() , "url":url}
-                    qa.append(new_qa)
-    elif add_your_logic in url: 
-        # Write here your logic to extract relevant informations based on documents        
-
-def get_question_answers_from_file(fileName,qa):
-    with open(fileName, 'r') as f:
-        json_string = f.read();
-        data = json.loads(json_string)
-        soup = BeautifulSoup(html.unescape(data["html_content"]), 'html.parser')
-        parse_question_answer_from_html(soup,qa)
-        
-               
+# This script find all the links on a web page an store them to a csv finle called urls_new.csv
 
 def main():
-    csvFile = input("Enter the urls of the sitemaps page : ");
-    qa =[];
-    with open(csvFile, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            time.sleep(.1);
-            get_question_answers_from_url(row[0],qa);
+    sitemaps = input("Enter the urls of the sitemaps page : ");
+    base_url = input("Enter the urls of the base url : ");
+    links =[];
 
-    with open("./inputs/urls.csv", "w") as f:
-        json.dump(qa, f)
-        
+    get_links_from_url(sitemaps,base_url,links);
+
+    with open("urls_new.csv", "w", newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in links:
+            writer.writerow(row)  
 if __name__ == '__main__':
     main()
 
